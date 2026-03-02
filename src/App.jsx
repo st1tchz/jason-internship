88f2390aa39bb8458fdaf93314337fa418225059
st1tchz <jason.allee@gmail.com>
2026-03-02 01:45:31 -0600

    Added aod animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Explore from "./pages/Explore";
 import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.refreshHard();
+  }, [location.pathname]);
+
   return (
-    <Router>
+    <>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,6 +27,24 @@ function App() {
         <Route path="/item-details/:nftId" element={<ItemDetails />} />
       </Routes>
       <Footer />
+    </>
+  );
+}
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 400,
+      once: false,
+      easing: "ease-in",
+      offset: 200,
+      mirror: true,
+    });
+  }, []);
+
+  return (
+    <Router>
+      <AnimatedRoutes />
     </Router>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,48 @@ import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
+import AOS from "aos";
 
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  useEffect(() => {
+    AOS.refreshHard();
+  }, []);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-        <Landing />
-        <LandingIntro />
-        <HotCollections />
-        <NewItems />
-        <TopSellers />
-        <BrowseByCategory />
+
+        <div data-aos="fade-up">
+          <Landing />
+        </div>
+
+        <div data-aos="fade-up" data-aos-delay="50">
+          <LandingIntro />
+        </div>
+
+        <div data-aos="fade-up" data-aos-delay="80">
+          <HotCollections />
+        </div>
+
+        <div data-aos="fade-up" data-aos-delay="110">
+          <NewItems />
+        </div>
+
+        <div data-aos="fade-right" data-aos-duration="900">
+          <TopSellers />
+        </div>
+
+        <div
+  data-aos="fade-left"
+  data-aos-duration="900"
+>
+  <BrowseByCategory />
+</div>
       </div>
     </div>
   );
